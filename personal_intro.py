86c697cc6b16cd23568b411d11534f578030aae4
Akash Chauhan <92942033+akashchauhan21@users.noreply.github.com>
2026-07-13 11:50:39 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,13 @@
+name = str(input("What is your name??"))
+print(name)
+
+
+age = int(input("How old are You??"))
+print(age)
+
+
+add = str(input("Where are you from??"))
+print(add)
+
+print(f"Welcome {name}!")
+print(f"You are {age} years old and leave in {add}")
